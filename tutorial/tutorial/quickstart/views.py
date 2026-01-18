b0f3c4e2391d05948131dc5df6e47bb7d362a4a0
Beast Code <beastcode9999@gmail.com>
2026-01-18 21:28:43 +0930

    feat: Add User and Group viewsets

--- tutorial/tutorial/quickstart/views.py
+++ tutorial/tutorial/quickstart/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group, User
 from rest_framework import permissions, viewsets
 
 # import serializers
 from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    # query set: What data am I working with
+    queryset = User.objects.all().order_by("-date_joined")
+    # Serializer_class: How do I format this data
+    serializer_class = UserSerializer
+    # Permission classes: Whos allowed to acces this
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
