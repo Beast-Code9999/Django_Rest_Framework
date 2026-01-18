ba8376f0f5ea174a4dbea440e474b4eafab93595
Beast Code <beastcode9999@gmail.com>
2026-01-18 20:38:20 +0930

    feat: Add User and Group serializers for API

--- tutorial/tutorial/quickstart/views.py
+++ tutorial/tutorial/quickstart/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import permissions, viewsets
 
+# import serializers
+from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
 # Create your views here.
